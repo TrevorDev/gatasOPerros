df717ec9a02e1ce23d08dbba745451b20ba7f372
Trevor Baron <trevorjbaron@gmail.com>
2017-03-19 21:53:32 -0700

    running imagenet

--- app.py
+++ app.py
@@ -3,50 +3,52 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import caffe
 
-print "started!"
+def createInputFromImage(imgPath):
+	#load image, resize
+	im = Image.open(imgPath).convert("RGB")
+	im = im.resize((224, 224), Image.ANTIALIAS)
 
-#load image, resize
-im = Image.open('cat.jpg').convert("RGB")
-im = im.resize((100, 100), Image.ANTIALIAS)
+	#convert to grayscale
+	#im = im.convert("L")
 
-#convert to grayscale
-im = im.convert("L")
+	ar = np.array(im)
+	print "ready image shape:"
+	print ar.shape
 
-ar = np.array(im)
-print "ready image shape:"
-print ar.shape
+	#separate each channel to b, g, r
+	res = np.array([ar[:,:,0],ar[:,:,1],ar[:,:,2]])
+	print "separated channel shape:"
+	print res.shape
 
-reshaped = ar[np.newaxis, np.newaxis, :, :]
-print "ready data shape:"
-print reshaped.shape
+	reshaped = res[np.newaxis, :, :]
+	print "ready data shape:"
+	print reshaped.shape
+	return reshaped
 
-print reshaped[0,0]
-net = caffe.Net('conv.prototxt', caffe.TEST)
+def createNet():
+	#create network from google's pretrained imagenet model
+	net = caffe.Net('caffeSource/models/bvlc_googlenet/deploy.prototxt',
+	                'caffeSource/models/bvlc_googlenet/bvlc_googlenet.caffemodel',
+	                caffe.TEST)
 
-net.blobs['data'].data[...] = reshaped
+	print "original net input shape:"
+	print net.blobs['data'].data[...].shape
 
-net.forward()
+	#reshape netowork input
+	net.blobs['data'].reshape(1,3,224,224)
+	print "reshapesd for batchsize of 1:"
+	print net.blobs['data'].data[...].shape
+	return net
 
-print net.blobs['conv'].data.shape
-print net.blobs['conv'].data[0,0]
+def main():
+	net = createNet()
+	net.blobs['data'].data[...] = createInputFromImage('images.jpg')
+	out = net.forward()
 
-newIm = Image.fromarray(net.blobs['conv'].data[0,0])
-newIm.show()
+	#print top catagory results
+	labels = np.loadtxt("words.txt", str, delimiter='\t')
+	top_k = net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1]
+	print labels[top_k]
 
-# im_ar = np.array(im)
-# reshaped = im_ar[np.newaxis, np.newaxis, :, :]
-# print reshaped.shape
 
-# 
-
-# im = np.array(Image.open('cat_gray.jpg'))
-# im_input = im[np.newaxis, np.newaxis, :, :]
-# print im_input.shape
-# net.blobs['data'].reshape(*im_input.shape)
-# net.blobs['data'].data[...] = im_input
-
-# net.forward()
-
-# print net.blobs['conv']
-# net.save('mymodel.caffemodel')
-# print "done!"
+main()
